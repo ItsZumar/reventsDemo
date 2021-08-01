5eed62f7909e47f835476643438174ad98a8cf7a
ItsZumar <zumarsaeed17@gmail.com>
2021-08-01 19:43:13 +0500

    first commit

---
+++ src/app/layout/App.js
@@ -0,0 +1,12 @@
+import { Typography } from "@material-ui/core";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Typography>Hi</Typography>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./app/layout/App";
+
+const rootEl = document.getElementById("root");
+
+let render = () => {
+  ReactDOM.render(<App />, rootEl);
+};
+
+if (module.hot) {
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
+}
+
+render();
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
